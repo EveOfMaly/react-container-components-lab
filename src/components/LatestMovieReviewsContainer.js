2d634aa920c0c196d7eceb30fe46519b33b77589
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-06-17 14:48:40 -1000

    First commit -m Setup test after setup issues

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+
+const NYT_API_KEY = 'hkCjUeuALx9iFKcyOzp7LvnDtndnBwLI';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+export default class LatestMovieReviewsContainer extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            test:"test"
+        }
+    }
+
+
+
+    render(){
+        return (<div></div>)
+    }
+}
+
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+const NYT_API_KEY = 'hkCjUeuALx9iFKcyOzp7LvnDtndnBwLI';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            test:"test"
+        }
+    }
+
+
+
+    render(){
+        return (<div></div>)
+    }
+}
+
+
